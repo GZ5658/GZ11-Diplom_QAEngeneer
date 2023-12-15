e410e666157a0e7eb510615231ec42e517c466c3
GZ5658 <151461626+GZ5658@users.noreply.github.com>
2023-12-16 00:51:31 +0300

    Удаление лишних строк

--- configuration.py
+++ configuration.py
@@ -1,7 +1,3 @@
 URL_SERVICE = "https://adf66273-c894-49c3-b5a4-9e8493652d0b.serverhub.praktikum-services.ru" #адрес стенда
-DOC_PATH = "/docs/" #документация API
 CREATE_ORDERS = "/api/v1/orders" #создание заказ
 ORDERS_INFO = "/api/v1/orders/track/" #информация о заказе
-
-#CREATE_USER_PATH = "/api/v1/users/"
-#CREATE_USER_KIT_PATH = "/api/v1/kits/"
